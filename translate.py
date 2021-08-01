0938f1273cac07f0ba63a0513aa9a3e153c05b70
Khaledk212@gmail.com <khaledk212@gmail.com>
2021-08-01 16:55:37 +0200

    first commit

---
+++ translate.py
@@ -0,0 +1,39 @@
+import requests, uuid, json
+
+# Add your subscription key and endpoint
+subscription_key = "subscription_key"
+endpoint = "https://api.cognitive.microsofttranslator.com"
+
+# Add your location, also known as region. The default is global.
+# This is required if using a Cognitive Services resource.
+location = "location(ex: eastus)"
+
+path = '/translate'
+constructed_url = endpoint + path
+
+params = {
+    'api-version': '3.0',
+    #you can choose any languages here
+    #here is the list: https://docs.microsoft.com/en-us/azure/cognitive-services/translator/language-support
+    'from': 'en',
+    'to': ['ar', 'de', 'fr']
+}
+constructed_url = endpoint + path
+
+headers = {
+    'Ocp-Apim-Subscription-Key': subscription_key,
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+# You can pass more than one object in body.
+text = input("Enter the text you want to translate:\n")
+body = [{
+    'text': text
+}]
+
+request = requests.post(constructed_url, params=params, headers=headers, json=body)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
